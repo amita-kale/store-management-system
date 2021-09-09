5558a936d54851704bbce9475e94602befb6a8f3
Amita kale <amitakale297@gmail.com>
2021-09-09 17:44:53 +0530

    login works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./login/login.module').then((m) => m.LoginModule),
   },
+
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
 ];
 
 @NgModule({
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginuser = {
+    username: '',
+    password: '',
+  };
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  submitClicked() {
+    if (
+      this.loginuser.username == 'amita' &&
+      this.loginuser.password == 'Smiler@123'
+    ) {
+      this.router.navigate(['home/home']);
+    }
   }
-
 }
